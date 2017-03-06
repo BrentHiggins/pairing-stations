e6e3b3ffc75124607085ed95801d5f00e0a5118e
Stuart Stanley <Stuart.Stanley@emc.com>
2017-03-06 15:43:27 -0600

    Baseline commit for setting up pairing stations via ansible. Baseline abilities are: * playbook to key stations using the current users ssh key * playbook to setup pairing software and settings ** git and such ** chrome ** terminator tool ** atom editor ** "go" langauge and some tools ** docker ** vagrant ** microsoft code studio editor ** bash-it tool (and config) ** git-duet with people listed ** various bash shortcuts ** desktop settings
    Items after this checkpoint:
    * write up readme (with any notable 'todo' items)
    * turn back on the whole inventory :)

---
+++ playbooks/roles/bash-it/files/custom/custom.aliases.bash
@@ -0,0 +1,10 @@
+
+#  Copyright (c) 2017 Dell Inc. or its subsidiaries. All Rights Reserved.
+alias dci='git duet-commit'
+alias gst='git status'
+
+alias please='sudo $(history -p !!)'
+
+alias ll='ls -AlGh'
+alias la='ls -a'
+alias l='ls -lGh'
